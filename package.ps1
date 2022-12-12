cbe74079a5fd486278cd56a7e304171d3d51c6ee
Dmitriy Krayniy <dkrayniy@entinco.com>
2022-12-12 19:40:17 +0400

    Updated pipeline

--- package.ps1
+++ package.ps1
@@ -3,31 +3,83 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-# Get component data and set necessary variables
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-$stageImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-rc"
-$latestImage="$($component.registry)/$($component.name):latest"
+# Get component metadata and set necessary variables
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$rcImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)"
+$latestImage = "$($component.registry)/$($component.name):latest"
+
+# Copy private keys to access git repo
+if (-not (Test-Path -Path "$PSScriptRoot/docker/id_rsa")) {
+    if (-not [string]::IsNullOrEmpty($env:GIT_PRIVATE_KEY)) {
+        Write-Host "Creating docker/id_rsa from environment variable..."
+        Set-Content -Path "$PSScriptRoot/docker/id_rsa" -Value $env:GIT_PRIVATE_KEY
+    } elseif (Test-Path -Path "~/.ssh/id_rsa") {
+        Write-Host "Copying ~/.ssh/id_rsa to docker..."
+        Copy-Item -Path "~/.ssh/id_rsa" -Destination "docker"
+    } else {
+        Write-Host "Missing ~/.ssh/id_rsa file..."
+        Set-Content -Path "$PSScriptRoot/docker/id_rsa" -Value ""
+    }
+}
 
 # Build docker image
-docker build -f docker/Dockerfile -t $stageImage -t $latestImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile" -t $rcImage -t $latestImage .
+if ($LastExitCode -eq 0) {
+    Write-Host "`nBuilt run images:`n$rcImage`n$latestImage`n"
+}
 
 # Set environment variables
-$env:IMAGE = $stageImage
+$env:IMAGE = $rcImage
+
+# Set docker ip
+if ($env:DOCKER_IP -ne $null) {
+    $dockerMachineIp = $env:DOCKER_IP
+} else {
+    $dockerMachineIp = "localhost"
+}
+
+# Set http port if default value overwritten
+if ($env:HTTP_PORT -ne $null) {
+    $httpPort = $env:HTTP_PORT
+} else {
+    $httpPort = "8080"
+}
+
+# Set http route to test container
+if ($env:HTTP_ROUTE -ne $null) {
+    $httpRoute = $env:HTTP_ROUTE
+} else {
+    $httpRoute = "/heartbeat"
+}
 
 try {
     # Workaround to remove dangling images
-    docker-compose -f ./docker/docker-compose.yml down
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" down
 
-    docker-compose -f ./docker/docker-compose.yml up -d
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" up -d
 
     # Test using curl
     Start-Sleep -Seconds 10
-    Invoke-WebRequest -Uri http://localhost:8080/heartbeat
-    #$postParams = @{ user_id="123", password="pwd123" }
-    #Invoke-WebRequest -Uri http://localhost:8080/v1/passwords/set_password -Method POST -Body $postParams
+    Invoke-WebRequest -Uri "http://$dockerMachineIp`:$httpPort$httpRoute"
+    #Invoke-WebRequest -Uri "http://$dockerMachineIp`:$httpPort/v1/microfrontends/get_microfrontends"
 
-    Write-Host "The container was successfully built."
-} finally {
+    if ($LastExitCode -eq 0) {
+        Write-Host "The run container was successfully built and tested."
+    }
+}   
+catch {
+    # Output container logs if web request failed
+    $containersStatuses = docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" ps
+    # Parse docker-compose list of containers
+    foreach ($containerStatus in $containersStatuses | Select -Skip 2) {
+        $containerName = $containerStatus.split(" ")[0]
+        Write-Host "`nLogs of '$containerName' container:"
+        docker logs $containerName
+    }
+    
+    Write-Error "Error on testing run container. See logs above for more information"
+}
+finally {
     # Workaround to remove dangling images
-    docker-compose -f ./docker/docker-compose.yml down
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" down
 }
